259193505bc8e7477f07c7c364b8bb1d5e6649ab
Milan Lesichkov <lesichkovm@gmail.com>
2022-09-11 14:46:22 +0100

    Update NewDashboard.go

--- NewDashboard.go
+++ NewDashboard.go
@@ -2,6 +2,12 @@ package dashboard
 
 func NewDashboard(config Config) Dashboard {
 	dashboard := Dashboard{}
+	dashboard.Title = config.Title
+	dashboard.Content = config.Content
+	dashboard.Scripts = config.Scripts
+	dashboard.ScriptURLs = config.ScriptURLs
+	dashboard.Styles = config.Styles
+	dashboard.StyleURLs = config.StyleURLs
 	dashboard.menu = config.Menu
 	dashboard.useSmartMenu = config.UseSmartMenu
 	dashboard.useMetisMenu = config.UseMetisMenu
