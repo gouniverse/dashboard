95214ba461d97a995e6bf91f769fe10971719e42
Milan Lesichkov <lesichkovm@gmail.com>
2024-01-14 22:37:08 +0000

    Fixed tests

--- NewDashboard_test.go
+++ NewDashboard_test.go
@@ -21,7 +21,7 @@ func TestNotFoundController(t *testing.T) {
 		"</head>",
 		"<body>",
 		"</body>",
-		`<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css" rel="stylesheet" />`,
+		`<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet" />`,
 		`<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" />`,
 		`<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>`,
 		`<link href="data:image/x-icon;base64,`,
